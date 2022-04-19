7c7b5448238b6b3f7212c0046823645251b4db7b
juancaarguelles <juan.aguelles@globant.com>
2022-04-19 12:59:52 -0500

    first point finished

---
+++ src/test/java/com/globant/FirstPoint.java
@@ -0,0 +1,60 @@
+package com.globant;
+
+
+import org.testng.annotations.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class FirstPoint
+{
+    @BeforeSuite
+    public void setUpDrive()
+    {
+        System.out.println("Setting up the Browser Driver");
+    }
+
+
+
+    @Test(priority = 0)
+    @Parameters({"username", "password"})
+    public void login(String username, String password)
+    {
+        System.out.println("Login with credentials : \n" +
+                "Username : "+username+" Password : "+password);
+    }
+
+    @Test( priority = 1,description = "Check if we go to friends section")
+    public void goToFriends()
+    {
+        System.out.println("Going to friends section");
+    }
+
+    @Test(priority = 2, description = "Here we select a friend")
+    @Parameters({"friend"})
+    public void selectFriend(String friend)
+    {
+        System.out.println("Friend : "+friend+" selected");
+    }
+
+    @Test(priority = 3, description = "Delete friend")
+    @Parameters({"friend"})
+    public void deleteFriend(String friend)
+    {
+        System.out.println("Friend : "+friend+" was deleted");
+    }
+
+    @Test(priority = 4, description = "Log out")
+    @Parameters({"username"})
+    public void logout(String username)
+    {
+        System.out.println(username+" has logged out");
+    }
+
+    @AfterClass
+    public void saveLogs()
+    {
+        System.out.println("Recorded logs were saved");
+    }
+
+}
---
+++ src/test/java/com/globant/SecondPoint.java
@@ -0,0 +1,12 @@
+package com.globant;
+
+import org.testng.annotations.Test;
+
+public class SecondPoint
+{
+    @Test
+    public void secondPoint()
+    {
+        System.out.println("The second point is here!");
+    }
+}
